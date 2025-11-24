78256bb2700beeb49e5db5e0bdcf1fdc402252ff
Margarita Zhuk <zhukrita@gmail.com>
2025-11-24 18:07:58 +0300

    amend 2 after comments

--- src/test/java/allureSteps/IssueInGithubSteps.java
+++ src/test/java/allureSteps/IssueInGithubSteps.java
@@ -1,10 +1,7 @@
 package allureSteps;
 
 import com.codeborne.selenide.*;
-import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
@@ -16,12 +13,6 @@ public class IssueInGithubSteps {
     public static SelenideElement issueTitleSelector = $("a[href='/MargaritaZhuk/Project_HW9/issues/1']");
     public static final String issue_number = "#1";
     public static ElementsCollection issueNumber = $$("[data-testid=list-row-repo-name-and-number]");
-
-
-    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
-    public byte[] takeScreenshot() {
-        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
-    }
 
     @Step("Открыть страницу")
     public void openPage() {
@@ -41,6 +32,5 @@ public class IssueInGithubSteps {
     @Step("Проверить, что есть ISSUE с указанным названием")
     public void checkIssueTitle(){
        issueTitleSelector.shouldHave(exactText(issue));
-       takeScreenshot();
     }
 }
---
+++ src/test/java/helpers/Attach.java
@@ -0,0 +1,13 @@
+package helpers;
+
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Attach {
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public static byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+}
